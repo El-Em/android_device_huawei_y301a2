a7c7b62a705115f4904e1336959ce61fa2d86de3
KainXS <lonniemonk@gmail.com>
2014-07-07 11:05:29 -0400

    just some rearranging

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,9 +51,9 @@ TARGET_KERNEL_SOURCE := kernel/huawei/y301a2
 TARGET_KERNEL_CONFIG := hw_msm8930_defconfig
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/huawei/y301a2/recovery/etc/fstab.y301a2
-BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/y301a2/recovery/graphics/graphics.c
-TARGET_RECOVERY_INITRC := device/huawei/y301a2/recovery/init.rc
+TARGET_RECOVERY_FSTAB := device/huawei/y301a2/rootdir/recovery/etc/fstab.y301a2
+BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/y301a2/recovery/graphics.c
+TARGET_RECOVERY_INITRC := device/huawei/y301a2/rootdir/recovery/init.rc
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -87,7 +87,7 @@ TW_INTERNAL_STORAGE_MOUNT_POINT := "internal_sd"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/y301a2/recovery/graphics/twrpgraphics.c
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/y301a2/recovery/twrpgraphics.c
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 
--- y301a2.mk
+++ y301a2.mk
@@ -20,57 +20,57 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/init.qcom.class_core.sh:recovery/root/init.qcom.class_core.sh \
-    $(LOCAL_PATH)/recovery/init.qcom.class_main.sh:recovery/root/init.qcom.class_main.sh \
-    $(LOCAL_PATH)/recovery/init.qcom.early_boot.sh:recovery/root/init.qcom.early_boot.sh \
-    $(LOCAL_PATH)/recovery/init.qcom.ril.sh:recovery/root/init.qcom.ril.sh\
-    $(LOCAL_PATH)/recovery/init.qcom.sh:recovery/root/init.qcom.sh \
-    $(LOCAL_PATH)/recovery/init.qcom.syspart_fixup.sh:recovery/root/init.qcom.syspart_fixup.sh \
-    $(LOCAL_PATH)/recovery/init.qcom.usb.rc:recovery/root/init.qcom.usb.rc\
-    $(LOCAL_PATH)/recovery/init.qcom.usb.sh:recovery/root/init.qcom.usb.sh \
-    $(LOCAL_PATH)/recovery/ueventd.qcom.rc:recovery/root/ueventd.qcom.rc 
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.class_core.sh:recovery/root/init.qcom.class_core.sh \
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.class_main.sh:recovery/root/init.qcom.class_main.sh \
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.early_boot.sh:recovery/root/init.qcom.early_boot.sh \
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.ril.sh:recovery/root/init.qcom.ril.sh\
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.sh:recovery/root/init.qcom.sh \
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.syspart_fixup.sh:recovery/root/init.qcom.syspart_fixup.sh \
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.usb.rc:recovery/root/init.qcom.usb.rc\
+    $(LOCAL_PATH)/rootdir/recovery/init.qcom.usb.sh:recovery/root/init.qcom.usb.sh \
+    $(LOCAL_PATH)/rootdir/recovery/ueventd.qcom.rc:recovery/root/ueventd.qcom.rc 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+    $(LOCAL_PATH)/rootdir/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_01.png:recovery/root/res/images/480x800/battery_01.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_02.png:recovery/root/res/images/480x800/battery_02.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_03.png:recovery/root/res/images/480x800/battery_03.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_04.png:recovery/root/res/images/480x800/battery_04.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_05.png:recovery/root/res/images/480x800/battery_05.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_06.png:recovery/root/res/images/480x800/battery_06.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_07.png:recovery/root/res/images/480x800/battery_07.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_08.png:recovery/root/res/images/480x800/battery_08.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_09.png:recovery/root/res/images/480x800/battery_09.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_10.png:recovery/root/res/images/480x800/battery_10.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/battery_11.png:recovery/root/res/images/480x800/battery_11.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/bg.png:recovery/root/res/images/480x800/bg.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_00.png:recovery/root/res/images/480x800/number_00.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_01.png:recovery/root/res/images/480x800/number_01.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_02.png:recovery/root/res/images/480x800/number_02.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_03.png:recovery/root/res/images/480x800/number_03.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_04.png:recovery/root/res/images/480x800/number_04.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_05.png:recovery/root/res/images/480x800/number_05.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_06.png:recovery/root/res/images/480x800/number_06.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_07.png:recovery/root/res/images/480x800/number_07.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_08.png:recovery/root/res/images/480x800/number_08.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_09.png:recovery/root/res/images/480x800/number_09.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_10.png:recovery/root/res/images/480x800/number_10.png \
-    $(LOCAL_PATH)/recovery/res/images/480x800/number_wait.png:recovery/root/res/images/480x800/number_wait.png
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_01.png:recovery/root/res/images/480x800/battery_01.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_02.png:recovery/root/res/images/480x800/battery_02.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_03.png:recovery/root/res/images/480x800/battery_03.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_04.png:recovery/root/res/images/480x800/battery_04.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_05.png:recovery/root/res/images/480x800/battery_05.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_06.png:recovery/root/res/images/480x800/battery_06.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_07.png:recovery/root/res/images/480x800/battery_07.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_08.png:recovery/root/res/images/480x800/battery_08.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_09.png:recovery/root/res/images/480x800/battery_09.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_10.png:recovery/root/res/images/480x800/battery_10.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/battery_11.png:recovery/root/res/images/480x800/battery_11.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/bg.png:recovery/root/res/images/480x800/bg.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_00.png:recovery/root/res/images/480x800/number_00.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_01.png:recovery/root/res/images/480x800/number_01.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_02.png:recovery/root/res/images/480x800/number_02.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_03.png:recovery/root/res/images/480x800/number_03.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_04.png:recovery/root/res/images/480x800/number_04.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_05.png:recovery/root/res/images/480x800/number_05.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_06.png:recovery/root/res/images/480x800/number_06.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_07.png:recovery/root/res/images/480x800/number_07.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_08.png:recovery/root/res/images/480x800/number_08.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_09.png:recovery/root/res/images/480x800/number_09.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_10.png:recovery/root/res/images/480x800/number_10.png \
+    $(LOCAL_PATH)/rootdir/recovery/res/images/480x800/number_wait.png:recovery/root/res/images/480x800/number_wait.png
 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/sbin/charge:recovery/root/sbin/charge \
-    $(LOCAL_PATH)/recovery/sbin/charge.sh:recovery/root/sbin/charge.sh \
-    $(LOCAL_PATH)/recovery/sbin/libqmi_oem_main:recovery/root/sbin/libqmi_oem_main \
-    $(LOCAL_PATH)/recovery/sbin/resize2fs_s:recovery/root/sbin/resize2fs_s \
-    $(LOCAL_PATH)/recovery/sbin/rmt_oeminfo:recovery/root/sbin/rmt_oeminfo \
-    $(LOCAL_PATH)/recovery/sbin/rmt_storage:recovery/root/sbin/rmt_storage
+    $(LOCAL_PATH)/rootdir/recovery/sbin/charge:recovery/root/sbin/charge \
+    $(LOCAL_PATH)/rootdir/recovery/sbin/charge.sh:recovery/root/sbin/charge.sh \
+    $(LOCAL_PATH)/rootdir/recovery/sbin/libqmi_oem_main:recovery/root/sbin/libqmi_oem_main \
+    $(LOCAL_PATH)/rootdir/recovery/sbin/resize2fs_s:recovery/root/sbin/resize2fs_s \
+    $(LOCAL_PATH)/rootdir/recovery/sbin/rmt_oeminfo:recovery/root/sbin/rmt_oeminfo \
+    $(LOCAL_PATH)/rootdir/recovery/sbin/rmt_storage:recovery/root/sbin/rmt_storage
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/tp/1191601.img:recovery/root/tp/1191601.img \
-    $(LOCAL_PATH)/recovery/tp/1469183_btn.img:recovery/root/tp/1469183_btn.img 
+    $(LOCAL_PATH)/rootdir/recovery/tp/1191601.img:recovery/root/tp/1191601.img \
+    $(LOCAL_PATH)/rootdir/recovery/tp/1469183_btn.img:recovery/root/tp/1469183_btn.img 
 
 $(call inherit-product, build/target/product/full.mk)
 
